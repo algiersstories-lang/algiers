8fa2d64b2435c0c825c72f3971da9db073b71260
algiersstories-lang <algiersstories@gmail.com>
2026-05-09 15:31:27 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,10 @@
+const csvtojson = require("csvtojson");
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addDataExtension("csv", async (contents) => {
+    return await csvtojson().fromString(contents);
+  });
+
   return {
     dir: {
       input: ".",
